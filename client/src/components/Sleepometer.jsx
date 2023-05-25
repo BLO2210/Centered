24adf1c3ab8a6e653f9831f6407c7b642c7e6bd4
curranod <curranod840@gmail.com>
2023-05-24 19:01:20 -0500

    sleepometer

---
+++ client/src/components/Sleepometer.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import GaugeChart from 'react-gauge-chart'
+import axios from 'axios';
+import moment from 'moment';
+
+const SleepGauge = () => {
+    const [chartValue, setChartValue] = useState(0);
+    const userId = localStorage.getItem('userId'); // retrieve user ID from local storage
+
+    const fetchAndProcessData = async () => {
+        try {
+            const res = await axios.get(`http://localhost:8080/api/users/${userId}`);
+            const sleepRatings = res.data.moodRatings
+                .filter(rating => moment(rating.timestamp).isSame(new Date(), 'week')) // filter for ratings from this week
+                .map(rating => rating.sleepQuality); // extract the sleepQuality field
+
+            const totalSleep = sleepRatings.reduce((a, b) => a + b, 0);
+            const averageSleep = totalSleep / sleepRatings.length; // calculate average sleep
+
+            setChartValue(averageSleep / 10); // normalize average sleep (assuming sleepQuality is out of 10)
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    useEffect(() => {
+        fetchAndProcessData();
+    }, []);
+
+    return (
+        <div>
+            <h2>Average Sleep This Week</h2>
+            <GaugeChart id="gauge-chart" 
+                        nrOfLevels={10} 
+                        percent={chartValue}
+                        textColor={"#000000"}
+                        needleColor={"#345243"} 
+                        colors={["#FF5F6D", "#FFC371", "#3AA0FF"]} />
+        </div>
+    );
+};
+
+export default SleepGauge;
