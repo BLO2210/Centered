21fce0f50384089050039808a201d24e2a3cd73e
curranod <curranod840@gmail.com>
2023-06-02 17:31:47 -0500

    exercise step progression bar

--- client/src/components/Exercise.jsx
+++ client/src/components/Exercise.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import './Exercise.css'
 
 function ExerciseTracker() {
-  const [workoutDays, setWorkoutDays] = useState(0);
-  
+  const [days, setDays] = useState(Array(7).fill(false));
+  const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]; // an array to display the days of the week
+
   useEffect(() => {
     const userId = localStorage.getItem('userId');
-    
+
     fetch(`http://localhost:8080/api/users/${userId}`)
       .then((response) => response.json())
       .then((user) => {
@@ -13,25 +15,34 @@ function ExerciseTracker() {
         const startOfWeek = new Date(today.setDate(today.getDate() - today.getDay()));
         const endOfWeek = new Date(today.setDate(today.getDate() - today.getDay() + 6));
 
-        let workoutDaysCount = 0;
+        const daysTemp = [...days];
 
         user.moodRatings.forEach((moodRating) => {
           const moodRatingDate = new Date(moodRating.timestamp);
           if (moodRatingDate >= startOfWeek && moodRatingDate <= endOfWeek && moodRating.exercise) {
-            workoutDaysCount += 1;
+            const dayOfWeek = moodRatingDate.getDay();
+            daysTemp[dayOfWeek] = true;
           }
         });
 
-        setWorkoutDays(workoutDaysCount);
+        setDays(daysTemp);
       })
       .catch((error) => {
         console.error('Error:', error);
       });
   }, []);
-  
+
   return (
-    <div>
-      You've worked out {workoutDays} days this week.
+    <div className="exercise-tracker-card">
+      <h2>Exercise</h2>
+      <div className="week-container">
+        {days.map((day, index) => (
+          <div className="day-container" key={index}>
+            <div className={`circle ${day ? 'filled' : ''}`}></div>
+            <p>{daysOfWeek[index]}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
